d3d9733f138e47ed55ca3cbe3d892e7b4749dbbf
Barbara <rsouzabarbara@gmail.com>
2025-04-14 21:48:58 -0300

    correção query do pedido

--- dao/pedido_dao.py
+++ dao/pedido_dao.py
@@ -3,17 +3,21 @@ from db.conexao import conectar
 def inserir_pedido(pedido):
     with conectar() as conn:
         with conn.cursor() as cur:
+            
+            cur.execute("SELECT MAX(orderid) AS ultimo_id FROM NORTHWIND.orders")
+            ultimo_id = cur.fetchone()["ultimo_id"] or 0
+            novo_order_id = ultimo_id + 1
+            
             cur.execute("""
-                INSERT INTO orders (customer_id, employee_id, order_date)
-                VALUES (%s, %s, %s)
-                RETURNING order_id
-            """, (pedido.customer_id, pedido.employee_id, pedido.order_date))
-            order_id = cur.fetchone()["order_id"]
+                INSERT INTO northwind.orders (orderid, customerid, employeeid, orderdate)
+                VALUES (%s, %s, %s, %s)
+                RETURNING orderid
+            """, (novo_order_id,pedido.customer_id, pedido.employee_id, pedido.order_date))
 
             for item in pedido.itens:
                 cur.execute("""
-                    INSERT INTO order_details (order_id, product_id, quantity, unit_price)
+                    INSERT INTO northwind.order_details (orderid, productid, quantity, unitprice)
                     VALUES (%s, %s, %s, %s)
-                """, (order_id, item.product_id, item.quantity, item.unit_price))
+                """, (novo_order_id, item.product_id, item.quantity, item.unit_price))
 
         conn.commit()
